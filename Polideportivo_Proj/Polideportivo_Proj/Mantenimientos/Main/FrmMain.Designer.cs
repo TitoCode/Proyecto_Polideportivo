2ad58b5bb2d85e6effc2dab9adc1421a3c4c1a3c
Tefa21 <stmario.campos@icloud.com>
2020-08-04 11:36:31 -0600

    Header Form Main

--- Polideportivo_Proj/Polideportivo_Proj/Mantenimientos/Main/FrmMain.Designer.cs
+++ Polideportivo_Proj/Polideportivo_Proj/Mantenimientos/Main/FrmMain.Designer.cs
@@ -32,6 +32,7 @@
             this.BtnListadoE = new System.Windows.Forms.Button();
             this.BtnCalendario = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // BtnLogin
@@ -40,9 +41,9 @@
             this.BtnLogin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.BtnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnLogin.Location = new System.Drawing.Point(710, 337);
+            this.BtnLogin.Location = new System.Drawing.Point(827, 1);
             this.BtnLogin.Name = "BtnLogin";
-            this.BtnLogin.Size = new System.Drawing.Size(166, 36);
+            this.BtnLogin.Size = new System.Drawing.Size(166, 39);
             this.BtnLogin.TabIndex = 2;
             this.BtnLogin.Text = "Log in";
             this.BtnLogin.UseVisualStyleBackColor = true;
@@ -53,9 +54,9 @@
             this.BtnListadoE.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.BtnListadoE.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnListadoE.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnListadoE.Location = new System.Drawing.Point(493, 337);
+            this.BtnListadoE.Location = new System.Drawing.Point(610, 1);
             this.BtnListadoE.Name = "BtnListadoE";
-            this.BtnListadoE.Size = new System.Drawing.Size(211, 36);
+            this.BtnListadoE.Size = new System.Drawing.Size(211, 39);
             this.BtnListadoE.TabIndex = 3;
             this.BtnListadoE.Text = "Listados De Equipos";
             this.BtnListadoE.UseVisualStyleBackColor = true;
@@ -67,19 +68,23 @@
             this.BtnCalendario.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.BtnCalendario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnCalendario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnCalendario.Location = new System.Drawing.Point(276, 337);
+            this.BtnCalendario.Location = new System.Drawing.Point(393, 1);
             this.BtnCalendario.Name = "BtnCalendario";
-            this.BtnCalendario.Size = new System.Drawing.Size(211, 36);
+            this.BtnCalendario.Size = new System.Drawing.Size(211, 39);
             this.BtnCalendario.TabIndex = 4;
             this.BtnCalendario.Text = "Calendario";
             this.BtnCalendario.UseVisualStyleBackColor = true;
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.BtnLogin);
+            this.panel1.Controls.Add(this.BtnCalendario);
+            this.panel1.Controls.Add(this.BtnListadoE);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(993, 39);
+            this.panel1.Size = new System.Drawing.Size(993, 40);
             this.panel1.TabIndex = 5;
             // 
             // FrmMain
@@ -89,11 +94,9 @@
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(993, 494);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.BtnCalendario);
-            this.Controls.Add(this.BtnListadoE);
-            this.Controls.Add(this.BtnLogin);
             this.Name = "FrmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
