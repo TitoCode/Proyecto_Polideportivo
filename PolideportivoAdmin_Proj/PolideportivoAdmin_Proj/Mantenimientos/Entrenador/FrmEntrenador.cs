0e293a61e127f4d3bcc5a6a5b5f6342e0bcee075
Tefa21 <stmario.campos@icloud.com>
2020-08-08 00:21:54 -0600

    Diseño Menu con Alexander

--- PolideportivoAdmin_Proj/PolideportivoAdmin_Proj/Mantenimientos/Entrenador/FrmEntrenador.cs
+++ PolideportivoAdmin_Proj/PolideportivoAdmin_Proj/Mantenimientos/Entrenador/FrmEntrenador.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,36 @@ namespace PolideportivoAdmin_Proj.Mantenimientos.Entrenador
         public FrmEntrenador()
         {
             InitializeComponent();
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            button1.ForeColor = Color.FromArgb(10, 37, 73);
+        }
+
+        private void FrmEntrenador_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.ForeColor = DefaultForeColor;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
