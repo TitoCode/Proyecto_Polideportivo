1f3c150c0acc848cbb10a87af5f04c2ac4ec5d22
Tefa21 <stmario.campos@icloud.com>
2020-08-19 17:02:19 -0600

    Liempieza de Código Campeonatos

---
+++ PolideportivoAdmin_Proj/PolideportivoAdmin_Proj/Clases/ClsCampeonato/ClsDatosCampeonato.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolideportivoAdmin_Proj.Clases.ClsCampeonato
+{
+    public class ClsDatosCampeonato
+    {
+        public string ID_Campeonato { get; set; }
+        public string Nombre { get; set; }
+        public string No_Equipos { get; set; }
+        public string Sede { get; set; }
+        public string Deporte { get; set; }
+        public string FechaCreacion { get; set; }
+    }
+}
